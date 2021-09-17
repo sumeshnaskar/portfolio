e492462d31a28ca53e7ae9b4fd4093e16b49bb0f
Sumesh <sumeshnaskar27@gmail.com>
2021-09-17 23:04:31 +0530

    react skeleton

---
+++ src/global-style.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+//global styles 
+export const GlobalStyle = createGlobalStyle`
+
+    *{
+        box-sizing: border-box;
+    }
+
+    html, body{
+        margin: 0;
+        font-size: 16px;
+    }
+`
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App Component
+import App from './App';
+
+//Render App Component
+ReactDOM.render(<App/>, document.getElementById('root'))
+
