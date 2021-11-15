93fa582937cf3309c59b654a0c9e6f62fc800558
Sumesh <sumeshnaskar27@gmail.com>
2021-11-16 02:06:46 +0530

    about

---
+++ src/container/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function AboutContainer(){
+    return (
+        // <About.Title></About.Title>
+        <About.Text>
+            Hi! I am a software engineer with 2+ years of experience who enjoys coding with a 
+            mindset of a user rather than a developer. I started my journey with the backend and 
+            was always curious about the frontend.
+            I followed the HTML, CSS, and JS tradition before switching to React, 
+            and since then, I have been looking for ways to showcase my abilities. 
+            The projects in my portfolio and this website are all created using React. 
+        </About.Text>
+    )
+}
