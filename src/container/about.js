1fe4b8745350f75b482691e57144f1dbd157b1b5
Sumesh <sumeshnaskar27@gmail.com>
2021-12-29 22:21:36 +0530

    updated resume and about section

--- src/container/about.js
+++ src/container/about.js
@@ -6,10 +6,12 @@ export function AboutContainer(){
     return (
         <About>
             <About.Text>
-            Hi! I'm Sumesh Naskar a meticulous web developer with over 2 years of experience having passion for responsive 
-            website design and a firm believer in the mobile-first approach. Experienced working with 
-            teams to produce impactful, leading-edge websites that engage customers and deliver business results. 
-            Well-versed in design standards and user preferences. Want to hire, collab or connect? Find my contact below.
+            Hi! I'm Sumesh Naskar, a solution-driven Software Developer with over 2+ years of experience. 
+            Thrive in working in a fast-paced, high-tech environment with cross-functional teams using agile methodology.
+            In my current job, I am working with golang and python but, curiosity in frontend technologies led me to a new path.
+            I started upskilling myself in April 2021 and now I am very comfortable with designing and developing responsive websites
+            with React. I wish to work as React developer someday and bring a valuable contribution to the team in producing impactful, 
+            leading-edge applications that engage customers and deliver business results.
             </About.Text>
             <Element id='contact'/>
             <Intro.Arrow/>
